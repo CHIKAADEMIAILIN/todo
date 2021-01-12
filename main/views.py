f2e5876a75989bc8866baf444131d7384c78c7f6
Chynara Dzhaparova <d.chynara@gmail.com>
2021-01-12 13:48:26 +0300

    added third test page and URL

--- main/views.py
+++ main/views.py
@@ -7,4 +7,7 @@ def test(request):
     return render(request, "test.html")    
 
 def second(request):
-    return HttpResponse('Test 2 page')   
+    return HttpResponse('Test 2 page')  
+
+def third(request):
+    return HttpResponse('This is page test3')     
