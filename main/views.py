ef7382825cf8ea2bef6e3b9b0825308af7ae2079
Chynara Dzhaparova <d.chynara@gmail.com>
2021-01-19 21:34:35 +0300

    added todo list in test page

--- main/views.py
+++ main/views.py
@@ -4,7 +4,8 @@ def homepage(request):
     return render(request, "index.html") 
 
 def test(request):
-    return render(request, "test.html")    
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})    
 
 def second(request):
     return HttpResponse('Test 2 page')  
